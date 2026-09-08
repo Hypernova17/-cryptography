afedc97ccf69948ee22e8a927871035a731e41a1
MayEscobar <mayraescobarsantander@gmail.com>
2026-09-08 17:19:04 -0600

    Correccion y comentarios

--- Archivos Cifrados/cifradocesar.py
+++ Archivos Cifrados/cifradocesar.py
@@ -20,35 +20,52 @@ def coincide(intento, firma):
 
     return True
 
+#Lista de extensiones más comunes y sus firmas correspondientes 
+firmas = [
+    bytes.fromhex("89504e470d0a1a0a"),
+    b"%PDF",
+    b"PK\x03\x04",
+    bytes.fromhex("ffd8ff"),
+    b"GIF89a",
+    b"GIF87a",
+]
 
-def encontrar_shift(datos_cifrados):
+extensiones = [
+    "png",
+    "pdf",
+    "zip",
+    "jpg",
+    "gif",
+    "gif",
+]
+
+
+def encontrar_shift_extension(datos_cifrados):
+    """
+    Encuentra la llave necesaria para el cifrado cesar probando con las 256 posibles corrimientos 
+    y compara con las firmas ya conocidas. 
+    """
     primeros_bytes = datos_cifrados[:8]
     
-    firmas = [
-        bytes.fromhex("89504e470d0a1a0a"),
-        b"%PDF",
-        b"PK\x03\x04",
-        bytes.fromhex("ffd8ff"),
-        b"GIF89a",
-        b"GIF87a",
-
-    ]
-
     for k in range(256):
         intento = bytes((b - k) % 256 for b in primeros_bytes)
-        for firma in firmas:
-            if coincide(intento, firma):
-                print(f"Se encontró la llave = {k} -> {intento}")
-                return k 
+        for i in range(len(firmas)):
+            if coincide(intento, firmas[i]):
+                print(f"Se encontró la llave = {k} -> {intento} ({extensiones[i]})")
+                return k, extensiones[i]
     
-    return None
+    return None, None
 
+#Leer el archivo cifrado
 datos_cifrados = leer_archivo_cesar()
 print("Bytes cifrados:", datos_cifrados[:8])
 print("En hex:", datos_cifrados[:8].hex())
 
-shift = encontrar_shift(datos_cifrados)
-print("Shift encontrado:", shift)
+#Encuentra la llave y el corrimiento aplicado
+shift, extension = encontrar_shift_extension(datos_cifrados)
+print("Corrimiento encontrado:", shift)
+print("Extension original:" , extensiones)
+
 
 """
     Segunda Parte: Descifrar el archivo
@@ -56,20 +73,31 @@ print("Shift encontrado:", shift)
 
 def descifrar(datos_cifrados, shift):
     datos_descifrados = []
-
-    for byte in datos_cifrados: 
+    #Recorremos cada del archivo
+    for byte in datos_cifrados:
+        #Se resta el corrimiento que se le haya aplicado a cada byte  
         byte_descifrado = byte - shift 
 
+        #Si el resultado es negativo entonces aplicamos mod 256
         if byte_descifrado < 0:
             byte_descifrado = byte_descifrado + 256
 
+        #Volvemos a convertir la lista a bytes
         datos_descifrados.append(byte_descifrado)
 
     return bytes(datos_descifrados)
 
-datos_descifrados = descifrar(datos_cifrados, shift)
+if shift is None: 
+    print("No se encontró la llave necesaria para descifrar el archivo")
+else:
+    #Descifra todos los datos con el corrimiento encontrado 
+    datos_descifrados = descifrar(datos_cifrados, shift)
 
-with open('cesar_descifrado.jpg', 'wb') as f:
-    f.write(datos_descifrados)
+    #Se agrega al archivo descifrado la extension correspondiente
+    nombre_final = "cesar_descifrado." + extension
 
-print("Archivo descifrado en formato jpg")
+    with open(nombre_final, 'wb') as f:
+        f.write(datos_descifrados)
+
+    print("Archivo descifrado: ", nombre_final)
+
